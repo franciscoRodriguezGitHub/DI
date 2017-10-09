dd17f359294eaa2c2eb6a164217499b44658524b
franciscoRodriguezGitHub <fran99sevillista@gmail.com>
2017-10-09 14:27:12 +0200

    s
    s

---
+++ Canvas/Canvas/Views/BlankPage1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Canvas.Views
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+           }
+
+        private void btnAtras_Click(object sender, RoutedEventArgs e)
+        {
+            // this.Frame.Navigate(typeof(MainPage));
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.GoBack();
+        }
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen;
+            inkCanvas.InkPresenter.InputProcessingConfiguration.Mode = Windows.UI.Input.Inking.InkInputProcessingMode.Inking;
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.InkPresenter.InputProcessingConfiguration.Mode = Windows.UI.Input.Inking.InkInputProcessingMode.Erasing;
+        }
+
+
+
+
+    }
+}
